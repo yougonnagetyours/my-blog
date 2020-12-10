f313c3ed8f1bdafffdf0cb3f72bdf182ceeab2c4
Assembly13 <assemblyinstruction13@gmail.com>
2020-12-10 12:55:07 +0000

    change tittle 2

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 const siteMetadata = {
-    title: `yougonnagetyours`,
+    title: `coco jambo i do przodu`,
     siteUrl: `https://woody-blog.netlify.app/`,
     capitalizeTitleOnHome: false,
     logo: `/images/4x4.png`,
